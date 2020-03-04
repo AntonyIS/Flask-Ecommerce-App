9413b77b14f67ce7c46280ca48081d629f779cc5
AntonyIS <antonyshikubu@gmail.com>
2020-03-04 16:39:05 +0300

    Styled the product detail page

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,19 @@ from app import app
 @app.route('/home')
 def index():
     return render_template('index.html', title='Home page')
+
+
+@app.route('/mali/<int:mali_id>')
+def product_detail(mali_id):
+    return render_template('product_detail.html', title='Home page')
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('feedback.html', error=error, title='Antony Injila | 404'),404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('feedback.html', error=error), 500
